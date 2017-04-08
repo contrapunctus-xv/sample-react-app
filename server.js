ef284df2f9ef4b7114a7a3495cd0bd17519f16a1
Joseph Lim <josephlim@Josephs-MBP.fios-router.home>
2017-04-07 22:46:26 -0400

    add bookings endpoint to server

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const path = require('path');
 const gonebusy = require('gonebusy-nodejs-client');
 const Promise = require('bluebird').Promise;
 const url = require('url');
+const bodyParser = require('body-parser');
 
 gonebusy.configuration.BASEURI = 'https://sandbox.gonebusy.com/api/v1';
 const authorization = 'Token ad86866858ea7c256390f96f58c520df';
 const serviceId = 2010395226;
+const userId = 9309567258;
 
 const services = Promise.promisifyAll(gonebusy.ServicesController);
 const resources = Promise.promisifyAll(gonebusy.ResourcesController);
+const bookings = Promise.promisifyAll(gonebusy.BookingsController);
 
 
 const app = express();
+app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
     const staticPath = path.join(__dirname, '/public');
@@ -24,7 +28,8 @@ app.get('/service', (req, res) => {
         if(error) {
            console.log(error.errorMessage);
         } else {
-            res.send(success.service.resources);
+            console.log(success.service);
+            res.send(success.service);
         }
     });
 });
@@ -50,6 +55,25 @@ app.get('/slots', (req, res) => {
             console.log(error.errorMessage);
         } else {
             res.send(success.service.resources)
+        }
+    });
+});
+
+app.post('/bookings/new', (req, res) => {
+    const { date, time, duration, resourceId} = req.body;
+    const createBookingBody = {
+        date,
+        time,
+        duration,
+        resource_id: resourceId,
+        service_id: serviceId,
+        user_id: userId
+    };
+    bookings.createBooking({authorization, createBookingBody}, (error, success) => {
+        if(error) {
+            console.log(error.errorMessage);
+        } else {
+            res.send(success);
         }
     });
 });
